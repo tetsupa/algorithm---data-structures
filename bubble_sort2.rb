e7886edcbdf706f1eee09a1ebfa51dc14b588f60
Tetsuharu Kohkaki <tetsupa0427@gmail.com>
2011-11-05 15:08:24 +0900

    equal to sort1

---
+++ bubble_sort2.rb
@@ -0,0 +1,29 @@
+require File.join(File.dirname(__FILE__), %w[ lib spec_helper ])
+
+def bubble_sort1(base_num, sort_num)
+  array = irand_array(base_num, sort_num)
+  array.collect! {|v|;  v += 1 }
+
+  p 'Begin ARRAY'
+  p array
+
+  count = 0
+
+  until (count == sort_num-1) do
+    count = 0
+    sort_index = 1
+    
+    (sort_num-1).times do
+      if array[sort_index-1] < array[sort_index]
+        count += 1
+      else
+        array[sort_index-1], array[sort_index] = array[sort_index], array[sort_index-1]
+      end
+      sort_index += 1
+    end
+  end
+  p 'End ARRAY'
+  p array
+end
+
+bubble_sort1(10, 5)
